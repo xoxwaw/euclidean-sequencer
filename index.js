9ade2c232a6d12882639303126891db4fd10ab55
probably-not-porter <pelibby16@earlham.edu>
2019-02-03 14:13:24 -0500

    add html and css to project

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
+const express = require('express');
+var path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => {
   console.log('app listening on port 8080!')

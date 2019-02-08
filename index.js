90faed3d38ecfbf80e7572c5bb0772c7a4390cf3
Ethan <ehrount15@earlham.edu>
2019-02-08 16:11:38 -0500

    Commented out tone.js stuff in index.js

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-require("Tone", function(Tone){
+/*require("Tone", function(Tone){
     var synth = new Tone.MonoSynth();
     var seq = new Tone.Sequence(function(time, note){
 	console.log(note);
 //straight quater notes
 }, ["C4", "E4", "G4", "A4"], "4n");
-});
+});*/
+
+
 app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

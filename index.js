ffa1d1a9e9e37b421c930933fe46e8c2d8e01339
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-02-09 22:04:52 -0500

    merge branch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var path = require('path');
+require("midi");
 const app = express();
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -7,13 +8,28 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-require("Tone", function(Tone){
-    var synth = new Tone.MonoSynth();
-    var seq = new Tone.Sequence(function(time, note){
-	console.log(note);
-//straight quater notes
-}, ["C4", "E4", "G4", "A4"], "4n");
-});
+
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+			var delay = 0; // play one note every quarter second
+			var note = 50; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
+		}
+	});
+};
+
+
+
 app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

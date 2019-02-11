ffcde8565f1e05f12f41c771d3b4b0e5deec9bed
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-02-11 12:41:02 -0500

    add comment on sound.js

--- public/js/sound.js
+++ public/js/sound.js
@@ -1,41 +1,39 @@
-console.clear();
-
-// UPDATE: there is a problem in chrome with starting audio context
-//  before a user gesture. This fixes it.
+//phi
+//Feb 11
 document.documentElement.addEventListener('mousedown', () => {
   if (Tone.context.state !== 'running') Tone.context.resume();
-});
+}); //fix Chrome constraints when you have to trigger to play music
 
 const synths = [
   new Tone.Synth(),
   new Tone.Synth(),
   new Tone.Synth()
-];
+]; //synth initializtion
 
 synths[0].oscillator.type = 'triangle';
 synths[1].oscillator.type = 'sine';
 synths[2].oscillator.type = 'sawtooth';
 
-const gain = new Tone.Gain(0.6);
-gain.toMaster();
+const gain = new Tone.Gain(0.5);
+gain.toMaster();//gain volume
 
 synths.forEach(synth => synth.connect(gain));
 
-const $rows = [[0,1,0,1,0,1,0,1],[1,0,0,1,0,0,1,0],[1,0,0,0,1,0,0,0,0]],
-      notes = ['G5', 'E4', 'C3'];
+const rows = [[0,1,0,1,0,1,0,1],[1,0,0,1,0,0,1,0],[1,0,0,0,1,0,0,0,0]],
+      notes = ['G5', 'E4', 'C3'];//sample sequencer
 let index = 0;
 
-Tone.Transport.scheduleRepeat(repeat, '8n');
+Tone.Transport.scheduleRepeat(loop, '8n');
 Tone.Transport.start();
 
-function repeat(time) {
+function loop(time) {
   let step = index % 8;
-  for (let i = 0; i < $rows.length; i++) {
+  for (let i = 0; i < rows.length; i++) {
     let synth = synths[i],
         note = notes[i],
-        $row = $rows[i],
-        $input = $row[step];
-    if ($input == 1) synth.triggerAttackRelease(note, '8n', time);
+        row = rows[i],
+        input = row[step];
+    if (input == 1) synth.triggerAttackRelease(note, '8n', time);
   }
   index++;
 }

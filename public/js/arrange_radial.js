b419e8c3549da26b80ec22cbdf0269b7c889bbcf
probably-not-porter <pelibby16@earlham.edu>
2019-02-12 15:00:28 -0500

    add general settings in place of voice settings for pulses, connect to backend

--- public/js/arrange_radial.js
+++ public/js/arrange_radial.js
@@ -6,6 +6,7 @@ Porter L
 
     2 / 3 / 19 : Initial commit + add templates
     2 / 4 / 19 : Fix null error
+    2 / 12 / 19 : Add connections to backend and populate all function for general
 */
 console.log("arrange_radial loaded");
 var dot_template1 = document.createElement('div'); // template for dot to populate circles
@@ -18,12 +19,12 @@ var dot_template3 = document.createElement('div'); // template for dot to popula
 dot_template3.setAttribute('class', 'dot');
 dot_template3.setAttribute('id', 'dot3');
 
-function populateAll(n){
+function populateAll(n){  //change all three voices at the same time [ THIS IS THE ONE THAT SHOULD BE CALLED ON CHANGE ( FOR GENERAL )]
   populate(n, 'circle1', 1);
   populate(n, 'circle2', 2);
   populate(n, 'circle3', 3);
+  pulses()
 }
-
 function populate(n,p,m){ // populate circle with n dots
   var parent = document.getElementById(p);
   if (parent){
@@ -44,7 +45,6 @@ function populate(n,p,m){ // populate circle with n dots
     arrange(p); // call to arrange after each time the number of dots changes
   }
 }
-
 function arrange(p){ // arrange dots with euclidian spacing
   var parent = document.getElementById(p);
   var children = parent.getElementsByTagName('div');
@@ -59,3 +59,38 @@ function arrange(p){ // arrange dots with euclidian spacing
     children[x].style.right = coord_y+'px';
   }
 }
+function pulses(){
+  var parent1 = document.getElementById('circle1');
+  var children1 = parent1.getElementsByTagName('div');
+  for(var x = 0; x < cycles[0].length; x++){
+    if (cycles[0][x] == 1){
+      children1[x].setAttribute('class', 'dot_pulse');
+    }
+    else{
+      children1[x].setAttribute('class', 'dot');
+    }
+  }
+
+  var parent2 = document.getElementById('circle2');
+  var children2 = parent2.getElementsByTagName('div');
+  for(var x = 0; x < cycles[1].length; x++){
+    if (cycles[1][x] == 1){
+      children2[x].setAttribute('class', 'dot_pulse');
+    }
+    else{
+      children2[x].setAttribute('class', 'dot');
+    }
+  }
+
+  var parent3 = document.getElementById('circle3');
+  var children3 = parent3.getElementsByTagName('div');
+  for(var x = 0; x < cycles[2].length; x++){
+    if (cycles[2][x] == 1){
+      children3[x].setAttribute('class', 'dot_pulse');
+    }
+    else{
+      children3[x].setAttribute('class', 'dot');
+    }
+  }
+}
+console.log(cycles);

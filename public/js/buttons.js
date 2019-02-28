b5243a8d1b21770f2e5c03c92194cfe8f9c5d921
probably-not-porter <pelibby16@earlham.edu>
2019-02-27 21:58:45 -0500

    style + switch buttons to img inputs

---
+++ public/js/buttons.js
@@ -0,0 +1,16 @@
+var paused = true;
+
+
+function buttonPlay(){
+  console.log('play button press');
+  if (paused == false){
+    Tone.Transport.stop();
+    paused = true;
+  }else{
+    Tone.Transport.start();
+    paused = false;
+  }
+}
+function buttonGit(){
+  window.open('https://github.com/hungphi98/euclidean-sequencer');
+}

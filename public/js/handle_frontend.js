e15cf383105c8e33d777e115351b7ea97327c8c0
probably-not-porter <pelibby16@earlham.edu>
2019-03-01 16:45:26 -0500

    refactor front-end js

--- public/js/handle_frontend.js
+++ public/js/handle_frontend.js
@@ -97,6 +97,64 @@ function pulses() {
     }
 }
 
+//initialize 3 voices
+var voice1 = document.getElementById('circle1');
+var voice2 = document.getElementById('circle2');
+var voice3 = document.getElementById('circle3');
+
+
+function updatePitchWrapper(elem, pitch_num, voice_index) {
+    /*
+    takes input from the keyboard and send this data to sound.js
+    */
+    pitch_codes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+    octave_code = document.getElementById("octave_" + String(voice_index)).value;
+    pitch_out = pitch_codes[pitch_num] + String(octave_code);
+    updatePitch(pitch_out, voice_index - 1); //call to sound.js to change note in voice
+    // update keyboard to display choice
+    if (elem.classList.contains('active') == false) {
+        var container = elem.parentElement;
+        var children = container.children;
+        for (var i = 0; i < children.length; i++) {
+            children[i].classList.remove('active' + String(voice_index - 1));
+        }
+        elem.classList.add('active' + String(voice_index - 1));
+    }
+}
+function updatePulseLabel(id, value){ // update the label for the pulses per measure (by voice)
+  document.getElementById(id).innerHTML = "Pulses Per Measure ( " + String(value) + " )";
+}
+function updateOffsetLabel(id,value){ // update the label for the number of steps (global)
+  document.getElementById(id).innerHTML = "Voice Offset ( " + String(value) + " )";
+}
+function updateStepLabel(id,value){ // update the label for the number of steps (global)
+  document.getElementById(id).innerHTML = "Steps Per Measure ( " + String(value) + " )";
+}
+
+function updateColor(color, dot_index){ // updated the color for a given voice
+  let root = document.documentElement;
+  root.style.setProperty('--color_one', color); // change color of dot by voice number
+}
+
+// Functions for play and link buttons
+
+var paused = true;
+
+function buttonPlay() {
+    console.log('play button press');
+    if (paused == false) {
+        Tone.Transport.stop();
+        paused = true;
+    } else {
+        Tone.Transport.start();
+        paused = false;
+    }
+}
+
+function buttonGit() {
+    window.open('https://github.com/hungphi98/euclidean-sequencer');
+}
+
 window.onload = function() { // once page is loaded, run initial populate
     populateAll(); // on load populate with current values.
 }

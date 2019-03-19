69d6eac29b5f1ac019a6817756b6906f9b861502
probably-not-porter <pelibby16@earlham.edu>
2019-03-19 15:02:55 -0400

    add server favicon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
+const path = require('path');
+const favicon = require('serve-favicon');
+
 const app = express();
+app.use(favicon(path.join(__dirname,'public','img','icon.ico')));
+
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
     res.render("index",{
@@ -10,6 +13,7 @@ app.get('/', function(req, res) {
 });
 
 app.use(express.static(__dirname + '/public'));
+
 
 app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
